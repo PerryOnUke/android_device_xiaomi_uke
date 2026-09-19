edc0de16c392292c43cc3ca66fd2ad3067bdffb5
kuroringo90 <ak4700@gmx.com>
2026-09-19 19:52:24 +0200

    uke: tuned powerhint (VR off) + stock perfboostselection for tuning

--- device.mk
+++ device.mk
@@ -25,3 +25,7 @@ $(call inherit-product, vendor/xiaomi/uke/uke-vendor.mk)
 
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,144)
 $(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
+
+# Powerhint (uke-tuned, overrides QCOM pineapple default)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
